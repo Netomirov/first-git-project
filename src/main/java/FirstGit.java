cea3570bef3e50f36d1a11a5dc7afe49370bb6af
Your Name <you@example.com>
2023-11-30 16:51:52 +0600

    delete sout

--- src/main/java/FirstGit.java
+++ src/main/java/FirstGit.java
@@ -2,7 +2,7 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GET Develop");
         System.out.println("HELLO!");
-        System.out.println("It's terribly difficult");
+
 
         System.out.println("I'm happy");
         System.out.println("!!!!!");
